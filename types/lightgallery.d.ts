c900be5446ae19d1564cab68d35d9492a8e8ce4d
soportesrdigitalpro@gmail.com <kevinrobertotorresruiz@gmail.com>
2025-10-21 15:07:35 -0600

    Agregar declaraciones de tipos para archivos CSS de lightGallery

--- types/lightgallery.d.ts
+++ types/lightgallery.d.ts
@@ -34,3 +34,10 @@ declare module 'lg-video' {
   const lgVideo: any;
   export default lgVideo;
 }
+
+// Declaraciones para archivos CSS
+declare module 'lightgallery/css/lightgallery.css';
+declare module 'lightgallery/css/lg-zoom.css';
+declare module 'lightgallery/css/lg-thumbnail.css';
+declare module 'lightgallery/css/lg-autoplay.css';
+declare module 'lightgallery/css/lg-fullscreen.css';
